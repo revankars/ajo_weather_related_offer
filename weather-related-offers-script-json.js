b6cbcc386e264d95b13bfdfbb6c70d0aa6679349
Sainath Revankar <sainathrevankar1401@gmail.com>
2026-06-27 22:47:06 -0600

    Update weather-related-offers-script-json.js
    Surface path is updated

--- weather-related-offers-script-json.js
+++ weather-related-offers-script-json.js
@@ -39,7 +39,7 @@ function sendWeatherDataToAEP() {
           renderDecisions: true,
           personalization: {
             surfaces: [
-              "web://revankars.github.io/weather/weather-offers-json.html#offerContainer"
+              "web://revankars.github.io/ajo_weather_related_offer/weather-offers-json.html#offerContainer"
             ]
           },
           xdm: {
